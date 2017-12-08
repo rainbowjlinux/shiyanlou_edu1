08f998e66a6476ff50dd7e3b713a35b7c84bf326
wangjinlaishuai <wangjinlaishuai@163.com>
2017-12-08 15:51:05 +0800

    Add project alpha version
    1. use blueprint to create app
    2. use flask.config.from_object to choose configuration
    3. use SQLAlchemy to connect MySQL dabatase
    4. use macro and extends to html page
    
    Signed-off-by: wangjinlaishuai <wangjinlaishuai@163.com>

---
+++ project_alpha/simpledu/simpledu/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+# from simpledu.config import configs
+from simpledu.models import db
+from simpledu.config import DevelopmentConfig
+
+
+def register_all_blueprint(app):
+    from simpledu.handlers import front, course, admin
+    app.register_blueprint(front)
+    app.register_blueprint(course)
+    app.register_blueprint(admin)
+
+
+def create_app(config):
+    app = Flask(__name__)
+    # app.config.from_object(configs.get(config))
+    app.config.from_object(DevelopmentConfig)
+    db.init_app(app)
+    register_all_blueprint(app)
+    return app
